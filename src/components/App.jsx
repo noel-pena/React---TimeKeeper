df585296be24ca3de429db9612b1749adaf770a6
noel-pena <azulu88@hotmail.com>
2023-12-23 21:10:54 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App() {
+  const time = new Date().toLocaleTimeString("IT-it");
+
+  const [currentTime, getTime] = useState(false);
+
+  function handleClick() {
+    getTime(!currentTime);
+  }
+
+  setInterval(handleClick, 1000);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={handleClick}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
